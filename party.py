34089f61bc26461315a24696ea72aecd2e0c93fd
Dani Bagan Camaches <dani.bagan@nan-tic.com>
2025-10-07 12:52:08 +0200

    Added new grouping method 'Customer Payment Days' to sales invoices Task #175956

---
+++ party.py
@@ -0,0 +1,43 @@
+from trytond.pool import PoolMeta
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
+
+class Party(metaclass=PoolMeta):
+    __name__ = "party.party"
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        payment_days = ('customer_payment_days', 'Customer Payment Days')
+        if payment_days not in cls.sale_invoice_grouping_period.selection:
+            cls.sale_invoice_grouping_period.selection.append(payment_days)
+
+    @classmethod
+    def validate(cls, parties):
+        super().validate(parties)
+        for p in parties:
+            p.check_customer_payment_days()
+
+    def check_customer_payment_days(self):
+        if self.sale_invoice_grouping_period != 'customer_payment_days':
+            return
+        if not self.customer_payment_days:
+            raise UserError(
+                gettext(
+                    'sale_invoice_grouping_payment_days.msg_customer_payment_days_required',
+                    party=self.rec_name
+                )
+            )
+
+class PartySaleInvoiceGroupingMethod(metaclass=PoolMeta):
+    __name__ = 'party.party.sale_invoice_grouping_method'
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        payment_days = ('customer_payment_days', 'Customer Payment Days')
+        if payment_days not in cls.sale_invoice_grouping_period.selection:
+            cls.sale_invoice_grouping_period.selection.append(payment_days)
+
+
+
---
+++ sale.py
@@ -0,0 +1,68 @@
+from datetime import date as date_cls
+from dateutil.relativedelta import relativedelta
+from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
+
+def days_in_month(date):
+        return (date + relativedelta(day=31)).day
+
+class PaymentTermLine(metaclass=PoolMeta):
+    __name__ = 'account.invoice.payment_term.line'
+
+    @classmethod
+    def previous_payment_day(cls, date, payment_days=None):
+        if payment_days is None:
+            payment_days = Transaction().context.get('account_payment_days')
+        if payment_days:
+            assert isinstance(payment_days, list)
+            payment_days = sorted(payment_days)
+            found = False
+            for day in reversed(payment_days):
+                if date.day >= day:
+                    if day > days_in_month(date):
+                        day = days_in_month(date)
+                    date += relativedelta(day=day)
+                    found = True
+                    break
+            if not found:
+                day = payment_days[-1]
+                date += relativedelta(day=day, months=-1)
+        return date
+
+class Sale(metaclass=PoolMeta):
+    __name__ = 'sale.sale'
+
+    def _get_invoice_dates(self, date, period):
+        if period != 'customer_payment_days':
+            return super()._get_invoice_dates(date, period)
+        pool = Pool()
+        PTline = pool.get('account.invoice.payment_term.line')
+
+        today = date or date_cls.today()
+        month_first = today.replace(day=1)
+        month_last = (month_first + relativedelta(months=1)) - relativedelta(days=1)
+
+        raw = self.party.customer_payment_days or ""
+
+        norm_days = sorted({
+        min(max(int(d), 1), month_last.day)
+        for d in raw.split()
+        if d.isdigit()
+        })
+        if not norm_days:
+            return month_first, month_last
+
+        is_exact = (today.day in norm_days)
+
+        next_date = PTline.next_payment_day(today, norm_days)
+        prev_date = PTline.previous_payment_day(today, norm_days)
+
+        if is_exact:
+            next_strict = PTline.next_payment_day(today + relativedelta(days=1), norm_days)
+            start = today
+            end = next_strict if next_strict.month == today.month else month_last
+            return start, end
+
+        start = (prev_date + relativedelta(days=1)) if prev_date.month == today.month else month_first
+        end = next_date if next_date.month == today.month else month_last
+        return start, end
