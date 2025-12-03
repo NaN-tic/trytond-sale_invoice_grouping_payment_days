e15ba88c86bec385d0d28e4673b9a0d1fd61fd88
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2025-12-03 13:42:54 +0100

    Implemented new grouping method 'Customer Payment Days' to sales invoices, that uses the party's customer payment days to delimit the invoice grouping period and invoices. Task #175956
    * Added new grouping method 'Customer Payment Days' to sales invoices
    Task #175956
    
    * Replace date import with datetime module
    
    * Improved sale.py logic to handle customer payment days when grouping invoices.
    Task #175956
    
    * Minor on-development fixes. Some commented and nasty code. At the very least, a bit of cleanup will be needed. Task #175956
    
    * Forgot to save a file. Just a little code rearange. Task #175956
    
    * Add test scenario for sale_invoice_grouping_payment_days and fix method call in sale.py
    
    * Code and test cleanup
    Task #175956
    
    ---------
    
    Co-authored-by: Dani Bagan Camaches <dani.bagan@nan-tic.com>
    Co-authored-by: Albert Cervera i Areny <albert@nan-tic.com>
    Co-authored-by: Kevin Ortega Garcia <kevin.ortega@nan-tic.com>

---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part sale_invoice_grouping_payment_days module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class SaleInvoiceGroupingPaymentDaysTestCase(ModuleTestCase):
+    'Test Sale Invoice Grouping Payment Days module'
+    module = 'sale_invoice_grouping_payment_days'
+
+del ModuleTestCase
